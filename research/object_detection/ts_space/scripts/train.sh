d48ab95bc21c1e33c48a8a47f19d86a32e1cda83
Ubuntu <azureuser@ts-ncv3-02.3mcb1mvwvn5ezkxiirlqgexqef.bx.internal.cloudapp.net>
2021-11-15 07:34:36 +0000

    Update config for mobilenet

--- research/object_detection/ts_space/scripts/train.sh
+++ research/object_detection/ts_space/scripts/train.sh
@@ -1,3 +1,4 @@
-python /home/ubuntu/work/tf_obj_det/research/object_detection/model_main.py --pipeline_config_path=/home/ubuntu/work/tf_obj_det/research/object_detection/ts_space/configs/ssd_inception_v2_castor.config --model_dir=/home/ubuntu/work/tf_obj_det/research/object_detection/ts_space/trained_models
+export PYTHONPATH=/home/azureuser/work/cd_expts/tf_obj_det/research/slim:/home/azureuser/work/cd_expts/tf_obj_det/research
+python /home/azureuser/work/cd_expts/tf_obj_det/research/object_detection/model_main_tf2.py --pipeline_config_path=/home/azureuser/work/cd_expts/tf_obj_det/research/object_detection/ts_space/configs/faster_rcnn_resnet101_v1_1024x1024_ts_cotton_tpu-8.config --model_dir=/mnt/ts-cvml-datastore/ts_model/models/cotton --checkpoint_every_n 1000 --wb_group FRC_NV1_009
 #python3 object_detection/model_main.py --pipeline_config_path=/home/ubuntu/object_detection_api/train_config/ssd_resnet50_v1_fpn_shared_box_predictor_640x640_coco14_sync.config --model_dir=/home/ubuntu/object_detection_api/detection_benchmark/
 #sudo poweroff
